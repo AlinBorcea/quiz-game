08a9d63da70b392dfe70b3238dabf852915ea248
alin <alinborcea2@gmail.com>
2022-02-10 15:39:37 +0200

    added tests for recordFromRaw

--- quiz/quiz_test.go
+++ quiz/quiz_test.go
@@ -2,6 +2,48 @@ package quiz
 
 import "testing"
 
+func Test_recordFromRaw_EmptyRecord(t *testing.T) {
+	var rec []string
+	if _, err := recordFromRaw(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_recordFromRaw_NoCorrect(t *testing.T) {
+	rec := []string{"QWe", "das,ads", ""}
+	if _, err := recordFromRaw(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_recordFromRaw_CorrectIsChar(t *testing.T) {
+	rec := []string{"QWe", "das,ads", "g"}
+	if _, err := recordFromRaw(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_recordFromRaw_CorrectIsString(t *testing.T) {
+	rec := []string{"QWe", "das,ads", "ghio"}
+	if _, err := recordFromRaw(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_recordFromRaw_CorrectIsSpecialChars(t *testing.T) {
+	rec := []string{"QWe", "das,ads", "\n\r\t"}
+	if _, err := recordFromRaw(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_recordFromRaw_Good(t *testing.T) {
+	rec := []string{"QWe", "das,ads", "2"}
+	if _, err := recordFromRaw(rec); err != nil {
+		t.FailNow()
+	}
+}
+
 func Test_questionFromRawRecord_Empty(t *testing.T) {
 	rec := []string{}
 	if _, err := questionFromRawRecord(rec); err == nil {
