a08b964ab182e2036fe089ed072509c223043d7e
alin <alinborcea2@gmail.com>
2022-02-10 15:21:11 +0200

    added tests for questionFromRawRecord

--- quiz/quiz_test.go
+++ quiz/quiz_test.go
@@ -2,6 +2,41 @@ package quiz
 
 import "testing"
 
+func Test_questionFromRawRecord_Empty(t *testing.T) {
+	rec := []string{}
+	if _, err := questionFromRawRecord(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_questionFromRawRecord_EmptyQue(t *testing.T) {
+	rec := []string{"", "ads,asd"}
+	if _, err := questionFromRawRecord(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_questionFromRawRecord_EmptyAnswers(t *testing.T) {
+	rec := []string{"que", ""}
+	if _, err := questionFromRawRecord(rec); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_questionFromRawRecord_AnswersNoComma(t *testing.T) {
+	rec := []string{"que", "1234"}
+	if _, err := questionFromRawRecord(rec); err != nil {
+		t.FailNow()
+	}
+}
+
+func Test_questionFromRawRecord_AnswersOnlyCommas(t *testing.T) {
+	rec := []string{"que", ",,,"}
+	if _, err := questionFromRawRecord(rec); err == nil {
+		t.FailNow()
+	}
+}
+
 func TestHasEmptyAnswer_Empty(t *testing.T) {
 	var answers []string
 
