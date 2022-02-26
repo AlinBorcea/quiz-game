3eaa0749aad01b76b25b255e0884ae0b7e95c872
alin <alinborcea2@gmail.com>
2022-02-26 14:53:50 +0200

    added tests for input and output

--- user/user.go
+++ user/user.go
@@ -1,1 +1,35 @@
 package user
+
+import (
+	"fmt"
+	"strings"
+)
+
+type User struct {
+	Name  string
+	Score int
+}
+
+func PostUserScore(user *User, filename string) error {
+	if !userIsValid(user) {
+		return fmt.Errorf("user %v is invalid", user)
+	}
+
+	if !strings.Contains(filename, ".csv") {
+		return fmt.Errorf("file %s is not a csv file", filename)
+	}
+
+	return nil
+}
+
+func userIsValid(user *User) bool {
+	if len(user.Name) <= 0 {
+		return false
+	}
+
+	if user.Score < 0 {
+		return false
+	}
+
+	return true
+}
--- user/user_test.go
+++ user/user_test.go
@@ -1,1 +1,37 @@
 package user
+
+import "testing"
+
+const scoreFilename = "scores.csv"
+
+func TestPostUserScore_EmptyUser(t *testing.T) {
+	user := User{Name: "", Score: 0}
+	if err := PostUserScore(&user, scoreFilename); err == nil {
+		t.Fatal("empty user is invalid")
+	}
+}
+
+func TestPostUserScore_NegativeScore(t *testing.T) {
+	user := User{Name: "alin", Score: -1}
+	if err := PostUserScore(&user, scoreFilename); err == nil {
+		t.Fatal("user score cannot be a negative integer")
+	}
+}
+
+func TestPostUserScore_EmptyFilename(t *testing.T) {
+	user := User{Name: "das", Score: 0}
+	if err := PostUserScore(&user, ""); err == nil {
+		t.Fatal("empty filename is invalid")
+	}
+}
+
+func TestPostUserScore_NotACsvFile(t *testing.T) {
+	user := User{Name: "asd", Score: 0}
+	if err := PostUserScore(&user, "some.txt"); err == nil {
+		t.Fatal("file is not a .csv file")
+	}
+}
+
+func Test_userIsValid(t *testing.T) {
+
+}
